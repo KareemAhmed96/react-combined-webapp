26e46a161cfbf21145ebeba4a2a111ef929e3e99
kareemzak <kareemahmed96@outlook.com>
2021-03-25 16:51:09 +0200

    created folder structure using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home'
+import Login from './components/Login'
+import ToDoApp from './components/ToDoApp'
+import Movies from './components/Movies'
+
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+class App extends React.Component {
+  
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/login">Login Link</Link>
+              </li>
+              <li>
+                <Link to="/todo">ToDo App Link</Link>
+              </li>
+              <li>
+                <Link to="/movies">Movies Link</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/* A <Switch> looks through its children <Route>s and
+              renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/movies">
+              <Movies />
+            </Route>
+            <Route path="/todo">
+              <ToDoApp />
+            </Route>
+            <Route path="/login">
+              <Login />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
+
 }
 
 export default App;
---
+++ src/components/ToDoApp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class ToDoApp extends React.Component {
+    render() {
+      return (
+        <div>
+          <h1>ToDo App Page</h1>
+        </div>
+      )
+    }
+}
+
+export default ToDoApp;
