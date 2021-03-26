177ca3f6cd8e68e535b0fb317680b4c25b3f7cf9
kareemzak <kareemahmed96@outlook.com>
2021-03-26 23:42:46 +0200

    websql attempt

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,6 +1,12 @@
 import React from 'react'
 import TodoForm from './TodoForm'
 import TodoList from './TodoList'
+import TodoWebSql from '../db/TodoWebSql'
+
+let db = TodoWebSql.dbInit()
+//TodoWebSql.query(db, "DROP TABLE items")
+let queryString = "CREATE TABLE if not exists items (id int primary key, task_text varchar(100), task_status varchar(100))"
+TodoWebSql.query(db, queryString)
 
 
 class TodoApp extends React.Component {
@@ -27,7 +33,13 @@ class TodoApp extends React.Component {
   }
 
   componentDidMount() {
-    
+    this.getTasks()
+  } 
+
+  getTasks = () => {
+    let selectQuery = "SELECT task_text FROM items"
+    let result = TodoWebSql.selectQuery(db, selectQuery)
+    console.log(result)
   }
 
   addTask = (task) => {
@@ -38,13 +50,20 @@ class TodoApp extends React.Component {
       id: this.id++
     }
 
+    let insertQuery = `INSERT INTO items(id, task_text, task_status) values("${newTask.id}", "${newTask.text}", "${newTask.done}")`
+    TodoWebSql.query(db, insertQuery)
+    this.getTasks()
+
     this.state.items.push(newTask)
     console.log(this.state.items)
     this.forceUpdate()
   }
 
   deleteTask = (taskId) => {
-    
+
+    let deleteQuery = `DELETE FROM items where id=${taskId}`
+    TodoWebSql.query(db, deleteQuery)
+
     let index = this.state.items.findIndex(
       (item) => item.id == taskId
     );
